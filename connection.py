59fb2fbdb7a75c9d5d6126b0b1bfc81000f6cbff
prashant799778 <prashantgoyal494@gmail.com>
2020-12-09 16:47:04 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import pymysql
 def DBconnection():
     mysqlcon = pymysql.connect(host='localhost',
                                 user='root',
-                                password='',
+                                password='#357A$de2#w',
                                 db='doctorapi',
                                 charset='utf8mb4',
                                 cursorclass=pymysql.cursors.DictCursor)
