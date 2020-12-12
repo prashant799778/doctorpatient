4122954b87cadc76f0aa95e29b6842414412fcc0
prashant799778 <prashantgoyal494@gmail.com>
2020-12-12 16:52:30 +0530

    Update docapp.py

--- docapp.py
+++ docapp.py
@@ -608,8 +608,9 @@ def patientlogin():
             userID = request.authorization["username"]
             password=request.authorization['password']
             column=  "*"
-            whereCondition= " and userID = '" + str(userID) + "'"
+            whereCondition= "and userID = '" + str(userID) + "'"
             loginuser=databasefile.SelectQuery1("patientMaster",column,whereCondition)
+            print(loginuser)
            
            
            
